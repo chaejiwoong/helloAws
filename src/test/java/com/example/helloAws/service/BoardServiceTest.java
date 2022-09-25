19c73861c1d2c49be3ec31ddb9cf7578e5760bb5
cowld <cowldnd2@naver.com>
2022-09-25 18:29:44 +0900

    123

--- src/test/java/com/example/helloAws/service/BoardServiceTest.java
+++ src/test/java/com/example/helloAws/service/BoardServiceTest.java
@@ -1,22 +1,22 @@
-package com.example.helloAws.service;
-
-import com.example.helloAws.domain.Board;
-import com.example.helloAws.web.dto.BoardDto;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootApplication
-class BoardServiceTest {
-
-    @Autowired
-    private BoardService boardService;
-
-    @Test
-    public void register() {
-
-    }
-
-}
+//package com.example.helloAws.service;
+//
+//import com.example.helloAws.domain.Board;
+//import com.example.helloAws.web.dto.BoardDto;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootApplication
+//class BoardServiceTest {
+//
+//    @Autowired
+//    private BoardService boardService;
+//
+//    @Test
+//    public void register() {
+//
+//    }
+//
+//}
